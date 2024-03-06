a8e287a5f0e97022eaf3325e4beef76905ae8437
Micha <michael.rigas@t-online.de>
2024-03-06 15:36:26 +0100

    AgeFunctions-Dateien

---
+++ Hausaufgabe-Tag3/AgeFunctions.test.js
@@ -0,0 +1,32 @@
+const { isAdult } = require("./AgeFunctions");
+
+describe("Tests for isAdult Function", () => {
+  test("Test for age being 18", () => {
+    expect(isAdult(18)).toBe(true);
+  });
+  test("Test for age being less than 0", () => {
+    expect(() => {
+      isAdult(-1);
+    }).toThrow("Age cannot be smaller than 0");
+  });
+
+  test("Test for age being 0", () => {
+    expect(isAdult(0)).toBe(false);
+  });
+
+  test("Test for age being less than 18", () => {
+    expect(isAdult(17)).toBe(false);
+  });
+
+  test("Test for age being greater than or equal to 18", () => {
+    expect(isAdult(18)).toBe(true);
+    expect(isAdult(19)).toBe(true);
+    expect(isAdult(100)).toBe(true);
+  });
+
+  test("Test for non-number input", () => {
+    expect(() => {
+      isAdult("not a number");
+    }).toThrow("Input is not a number");
+  });
+});
